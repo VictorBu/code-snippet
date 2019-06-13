d3412d7217e91aab246d1876b2833087df4ae963
victorbu <victorbu.xx@gmail.com>
2019-06-13 10:49:20 +0800

    Hystrix Dashboard

--- java/spring-cloud-parent/spring-cloud-eureka-feign-client/src/main/java/com/karonda/EurekaFeignClientApp.java
+++ java/spring-cloud-parent/spring-cloud-eureka-feign-client/src/main/java/com/karonda/EurekaFeignClientApp.java
@@ -2,10 +2,14 @@ package com.karonda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients // 开启 Feign Client
+@EnableHystrixDashboard // 启用 Hystrix Dashboard
+@EnableCircuitBreaker // 启用 Hystrix 熔断器
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaFeignClientApp {
--- java/spring-cloud-parent/spring-cloud-eureka-ribbon-client/src/main/java/com/karonda/EurekaRibbonClientApp.java
+++ java/spring-cloud-parent/spring-cloud-eureka-ribbon-client/src/main/java/com/karonda/EurekaRibbonClientApp.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableHystrix // 启用 Hystrix 熔断器
+@EnableHystrixDashboard // 启用 Hystrix Dashboard
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaRibbonClientApp {
